cae1abf4f54acfd45104792250e9abed3f91ea38
ans.ya <tech.academy.stu@gmail.com>
2019-06-02 06:38:39 +0000

    add colum

--- app/models/message.rb
+++ app/models/message.rb
@@ -1,3 +1,5 @@
 class Message < ApplicationRecord
     validates :content, presence: true, length: { maximum: 255 , too_long: "最大%{count}文字まで使えます。%{count}文字以内にしてください。" }
+    validates :title, presence: true, length: { maximum: 255 , too_long: "最大%{count}文字まで使えます。タイトルなので短めで！理想は20文字程度" }
+
 end
